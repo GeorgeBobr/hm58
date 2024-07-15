8bae2e5376aef4cee256453802db56826f0c2be0
Georg <georgestoneix@gmail.com>
2024-07-15 05:38:33 -0700

    Переделал путь для редактирования

--- issuetracker/webapp/urls.py
+++ issuetracker/webapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', IssueListView.as_view(), name='index'),
     path('Issue/<int:id>/', IssueDetailView.as_view(), name='issue_detail'),
     path('Issue/create/', IssueCreateView.as_view(), name='issue_create'),
-    path('Issue/update/<int:id>/', IssueUpdateView, name='issue_update'),
+    path('issue/update/<int:id>/', IssueUpdateView.as_view(), name='issue_update'),
     path('Issue/delete/<int:id>/', IssueDeleteView, name='issue_delete')
 ]
